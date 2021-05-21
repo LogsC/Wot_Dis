e65277b8d70c894e3bcc87bdacf7673e40995f3e
LogsC <logan.c.chiu@gmail.com>
2021-05-21 03:11:35 -0700

    update some more message testing, retrieval of old discord messages

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+const dotenv = require("dotenv");
+dotenv.config();
 // import discord.js and mongodb
 const Discord = require("discord.js");
-// const mongo = require("mongodb");
-
+const mongo = require("mongodb");
 // set up clients
 const client = new Discord.Client();
-// const MongoClient = mongo.MongoClient;
+const MongoClient = mongo.MongoClient;
 
 // set up uri to access mongoDB Atlas
-// const uri = "mongodb+srv://LogLogs:" + process.env.MDB_PWD + "@msgcluster.8dtqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://LogLogs:" + process.env.MDB_PWD + "@msgcluster.8dtqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// console.log("uri: " + uri);
 
 // log into discord bot account
 client.on("ready", () => {
@@ -20,12 +22,13 @@ client.on("ready", () => {
   console.log("Users: \n" + client.users.cache.array());
   console.log("Messages: \n");
   // obtain list of all channels that the bot (client) has access to
-  var channels = client.guilds.channels.cache.array();
+  var channels = client.channels.cache.array();
   // confirm first channel is a text channel
   if (channels[0].isText()) {
     var messages = channels[0].messages.cache.array();
   }
   // pull the 100 most recent messages from first channel in list
+  console.log("Channels: " + channels.length);
   channels[0].messages.fetch({ limit: 100 }).then(messages => {
     console.log(`Received ${messages.size} messages`);
     //Iterate through the messages here with the variable "messages".
@@ -42,48 +45,55 @@ var prefix = "~";
 
 // wait for message
 client.on("message", msg => {
-  var curr_msg = msg.content;
-  // print msg info
-  console.log("message: " + msg.content); // message string
-  console.log("words: " + msg.content.split(" ")); // array of words split by space " "
-  console.log("author: " + msg.author); // msg.author and msg.author.id return same number string (id)
-  console.log("msg ID: " + msg.id); // msg id
-  console.log("Timestamp: " + msg.createdTimestamp); // time (milliseconds) since 1/1/1970 00:00 UTC
-  console.log("Time: " + msg.createdAt); // time in legible form
-  console.log("Channel: " + msg.channel.id); // channel id
-  console.log("Guild: " + msg.guild.id); // guild (server) id
-  // if first char of message = "~"
-  if (curr_msg.substring(0, 1) == prefix) {
-    // split message String into array separated by spaces " "
-    var args = msg.content.substring(1).split(" ");
-    // set the command to be the first word - "~help" -> "help"
-    var cmd = args[0];
-    // remove cmd from args list
-    args = args.splice(1);
-    // process the command argument
-    switch(cmd) {
-      case "help":
-        msg.channel.send("Here are my commands!\n" +
-          "~wotlabs <username>");
-        break;
-      case "wotlabs":
-        msg.channel.send("https://wotlabs.net/na/player/" + args[0]);
-        break;
-      default:
-        // do not want to spam if no command called
+  // ignore all bot messages
+  if (!msg.author.bot) {
+    var curr_msg = msg.content;
+    // print msg info
+    console.log("message: " + msg.content); // message string
+    console.log("words: " + msg.content.split(" ")); // array of words split by space " "
+    console.log("author: " + msg.author); // msg.author and msg.author.id return same number string (id)
+    console.log("msg ID: " + msg.id); // msg id
+    console.log("Timestamp: " + msg.createdTimestamp); // time (milliseconds) since discord epoch (?)
+    console.log("Time: " + msg.createdAt); // time in legible form
+    console.log("Channel: " + msg.channel.id); // channel id
+    console.log("Guild: " + msg.guild.id); // guild (server) id
+    // if first char of message = "~"
+    if (curr_msg.substring(0, 1) == prefix) {
+      // split message String into array separated by whitespace
+      var args = msg.content.substring(1).split(/\s/);
+      // set the command to be the first word - "~help" -> "help"
+      var cmd = args[0];
+      // remove cmd from args list
+      args = args.splice(1);
+      // process the command argument
+      switch(cmd) {
+        case "help":
+          msg.channel.send("Here are my commands!\n" +
+            "~wotlabs <username>");
+          break;
+        case "wotlabs":
+          msg.channel.send("https://wotlabs.net/na/player/" + args[0]);
+          break;
+        default:
+          // do not want to spam if no command called
+      }
+    }
+    // omegalul
+    if (curr_msg.toLowerCase() == "bruh") {
+      msg.channel.send("bruh moment");
+    }
+    if (curr_msg.toLowerCase() == "ping") {
+      // check server lag?
+      msg.reply("Pong!");
+    }
+    // if message (after removing whitespace and upper case) == "james"
+    if (curr_msg.replace(/\s+/g, '').toLowerCase() == "james") {
+      msg.channel.send(curr_msg);
     }
   }
-  // omegalul
-  if (curr_msg.toLowerCase() == "bruh") {
-    msg.channel.send("bruh moment");
-  }
-  if (curr_msg.toLowerCase() == "ping") {
-    // check server lag?
-    msg.reply("Pong!");
-  }
 })
-
-client.login(process.env.TOKEN)
+// console.log(process.env.TOKEN);
+client.login(process.env.TOKEN);
 
 // MongoDB client connection reference (replace <password> with LogLogs password):
 /*
