ddabc28a7178fbf9092e5ba9d94b4660163117d8
LogsC <logan.c.chiu@gmail.com>
2021-05-24 02:43:36 -0700

    working discord wordcount data collection / access via MDB

--- index.js
+++ index.js
@@ -31,38 +31,47 @@ clientMDB.connect(err => {
     // obtain list of all channels that the bot (client) has access to
     var channels = client.channels.cache.array();
     console.log("Channels: " + channels.length);
-    // var textchannels = 0;
-    // var nummessages = 0;
+    var textchannels = 0;
+    var nummessages = 0;
     channels.forEach(channel => {
       // confirm channel is a text channel
       if (channel.isText()) {
-        // textchannels += 1;
-        var messages = extract_messages(channel);
-        messages.forEach(message => {
-          // filter out any bot messages
-          if (!message.author.bot) {
-            // if the current message ID already exists in DB, go next
-            var query = {"MessageID": message.id};
-            col.findOne(query, function(err, result) {
-              if (err) throw err;
-              // if there exists a result with given message ID, go next
-              if (!result) {
-                // split message into individual words
-                var words = message.content.split(/\s/);
-                words.forEach(word => {
-                  // create and insert messageData
-                  let wordData = {
-                    "Word": word,
-                    "Date": message.createdTimestamp,
-                    "MessageID": message.id,
-                    "UserID": message.author.id,
-                    "GuildID": message.guild.id
-                  }
-                  col.insertOne(wordData);
-                })
-              }
-            })
-          }
+        textchannels += 1;
+        // var messages = extract_messages(channel);
+        // TODO: swap out 100 cap with function
+        channel.messages.fetch({ limit: 100 }).then(messages => {
+          nummessages += messages.size;
+          messages.forEach(message => {
+            // filter out any bot messages
+            if (!message.author.bot) {
+              // if the current message ID already exists in DB, go next
+              var query = {"MessageID": message.id};
+              col.findOne(query, function(err, result) {
+                if (err) throw err;
+                // if there exists a result with given message ID, go next
+                if (!result) {
+                  // split message into individual words
+                  var words = message.content.split(/\s/);
+                  words.forEach(word => {
+                    // remove trivial words
+                    if (word != '') {
+                      // create and insert messageData
+                      let wordData = {
+                        "Word": word.toLowerCase(),
+                        "Date": message.createdTimestamp,
+                        "MessageID": message.id,
+                        "UserID": message.author.id,
+                        "GuildID": message.guild.id
+                      }
+                      col.insertOne(wordData);
+                    }
+                  })
+                }
+              })
+            }
+          })
+          console.log("Text channels: " + textchannels);
+          console.log("Message amount: " + nummessages);
         })
         // pull the 100 most recent messages from first channel in list
         /*
@@ -133,14 +142,43 @@ clientMDB.connect(err => {
           case "help":
             msg.channel.send("Here are my commands!\n" +
               "~wotlabs <username>\n" +
-              "~mdb <command / query>");
+              "~word <command / query>");
             break;
           case "wotlabs":
             msg.channel.send("https://wotlabs.net/na/player/" + args[0]);
             break;
-          case "mdb":
-            // args[0] = <command / query>
+          case "word":
+            // args = <command / query>
             // if args[0] is undefined, send out list of possible commands / queries
+            if (!args[0]) {
+              msg.channel.send("Current word commands include: \n" +
+                "~word amt <word>\n" +
+                "~word amt <word> server");
+            } else {
+              // can expand later
+              if (args[0] == "amt") {
+                var word = args[1].toLowerCase();
+                if (args[2] == "server") {
+                  // query is only limited to this server
+                  var query = { "Word": word, "GuildID": msg.guild.id };
+                  col.find(query).toArray(function(err, result) {
+                    if (err) throw err;
+                    var count = result.length;
+                    msg.channel.send("The amount of times " + word +
+                      " has been said in this server is: " + count);
+                  })
+                } else if (word) {
+                  var query = { "Word": word, "UserID": msg.author.id, "GuildID": msg.guild.id };
+                  col.find(query).toArray(function(err, result) {
+                    if (err) throw err;
+                    var count = result.length;
+                    msg.channel.send("The amount of times " + word +
+                    " has been said by " + msg.author.username +
+                    " in this server is: " + count);
+                  })
+                }
+              }
+            }
             break;
           default:
             // do not want to spam if no command called
@@ -167,7 +205,8 @@ clientMDB.connect(err => {
 /* function for obtaining discord messages
  * Credit: Jason, https://stackoverflow.com/questions/55153125/fetch-more-than-100-messages
  */
-async function extract_messages(channel, limit = 500) {
+/*
+async function extract_messages(channel, limit = 250) {
     const sum_messages = [];
     let last_id;
     while (true) {
@@ -175,7 +214,7 @@ async function extract_messages(channel, limit = 500) {
         if (last_id) {
             options.before = last_id;
         }
-        const messages = await channel.fetchMessages(options);
+        const messages = await channel.messages.fetch(options);
         sum_messages.push(...messages.array());
         last_id = messages.last().id;
         if (messages.size != 100 || sum_messages >= limit) {
@@ -184,3 +223,4 @@ async function extract_messages(channel, limit = 500) {
     }
     return sum_messages;
 }
+ */
